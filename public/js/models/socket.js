3aedd64965d50aea44f9b0f95143be736f4d0e16
melch <melch@ringrevenue.com>
2012-08-18 10:11:58 -0700

    whitespace

--- public/js/models/socket.js
+++ public/js/models/socket.js
@@ -5,18 +5,18 @@ var SocketWrapper = Backbone.Model.extend( {
         connected : false
       },
 
-      connect: function() { 
+      connect: function() {
           if ( socket && socket.readyState == 1 )
           {
               console.log( "Already connected" );
               return;
           }
-          
+
           var url = 'ws://' + this.get( 'host' ) + ':' + this.get( 'port');
           var self = this;
-          
+
           console.log( "Connecting to " + url );
-          
+
           socket = new WebSocket( url );
           this.set( { connected : true } );
 
@@ -35,7 +35,7 @@ var SocketWrapper = Backbone.Model.extend( {
                 self.trigger( "socket:parse_error", "Could not parse message from socket: " + msg.data );
                 return;
               }
-              
+
               if ( response.type == "Connect" )
               {
                   self.trigger( "socket:connect", response['data'] );
@@ -50,13 +50,13 @@ var SocketWrapper = Backbone.Model.extend( {
               self.set( { connected : false } );
               self.trigger( "socket:error" );
           };
-          
+
           socket.onclose = function(evt) {
               self.set( { connected : false } );
               console.log( evt );
           }
       },
-      
+
       disconnect: function() {
           if ( socket )
           {
