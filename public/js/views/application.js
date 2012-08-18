de7930a7d3bca329836c37ba6dcdeb3a8268502d
melch <melch@ringrevenue.com>
2012-08-17 21:05:25 -0700

    whitespace and indentation.

--- public/js/views/application.js
+++ public/js/views/application.js
@@ -1,27 +1,27 @@
 var App = {};
 
 App.Router = Backbone.Router.extend({
-    
-   // Hash maps for routes
-   routes : {
-      ""       : "lobby",
-      "game"   : "game",
-      "logout" : "logout"
-   },
-   
-   lobby: function(){
-       app.lobby.render();
-       app.viewchange( 'lobby' );
-   },
-   
-   game: function() {
-       app.game.render();
-       app.viewchange( 'game' );
-   },
-   
-   logout: function() {
-       alert( "Oh, did you expect that you could log out?" );
-   }
+
+  // Hash maps for routes
+  routes : {
+    ""       : "lobby",
+    "game"   : "game",
+    "logout" : "logout"
+  },
+
+  lobby: function(){
+    app.lobby.render();
+    app.viewchange( 'lobby' );
+  },
+
+  game: function() {
+    app.game.render();
+    app.viewchange( 'game' );
+  },
+
+  logout: function() {
+    alert( "Oh, did you expect that you could log out?" );
+  }
 
 });
 
@@ -29,22 +29,22 @@ App.MainView = Backbone.View.extend({
 
   initialize: function() {
     console.log( "initialize" );
-    
+
     this.current_view = null;
   },
-  
+
   viewchange: function( current_view ) {
-      this.current_view = current_view;
-      
-      // this stuff could be their own views or something
-      $('.view').hide();
-      $('#' + this.current_view ).show();
-      
-      // especially this nav logic
-      $('ul.nav li').removeClass( 'active' );
-      $('ul.nav li.' + current_view).addClass( 'active' );
+    this.current_view = current_view;
+
+    // this stuff could be their own views or something
+    $('.view').hide();
+    $('#' + this.current_view ).show();
+
+    // especially this nav logic
+    $('ul.nav li').removeClass( 'active' );
+    $('ul.nav li.' + current_view).addClass( 'active' );
   },
-  
+
   render: function() {
     this.lobby = new App.LobbyView({ el: this.$("#lobby") });
     this.game  = new App.GameView({ el: this.$("#game") });
@@ -55,20 +55,20 @@ App.LobbyView = Backbone.View.extend({
   template: _.template($("#application-lobby").html()),
 
   initialize: function() {
-      this.username = null;
+    this.username = null;
   },
-  
+
   login: function( username ) {
-      this.username = username;
-      console.log( "LOGIN: " + username );
-      
-      window.location.hash = 'game';
+    this.username = username;
+    console.log( "LOGIN: " + username );
+
+    window.location.hash = 'game';
   },
-  
+
   render: function() {
-      console.log( "render lobby view" );
+    console.log( "render lobby view" );
     $(this.el).html( this.template( this.model ) );
-    
+
     return this;
   }
 
@@ -80,7 +80,7 @@ App.GameView = Backbone.View.extend({
   render: function() {
     console.log( "render lobby view" );
     $(this.el).html( this.template( this.model ) );
-    
+
     return this;
   }
 
